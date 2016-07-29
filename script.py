5d9a10da79668890f766e81b0d58a064b8059ec4
Skeletonxf <skeletonxf@gmail.com>
2016-07-29 01:03:03 +0100

    additional fixes and comment merging

--- script.py
+++ script.py
@@ -63,15 +63,13 @@ else:
 
 print("about to search for comments to reply to")
 
-def replyToComment(comment,triggers,reply,flag):
+def add_reply_if_valid(comment,triggers,reply,flag):
     "This checks for a comment in the comments to reply to and then replies"
     if comment.id not in replied_to:
         for trigger in triggers:
             # search using regex for the trigger in the comment body
-            if re.search(trigger,comment.body,re.IGNORECASE):
-                comment.reply(reply+END_OF_REPLY_MSG)
-                print ("Bot replying to comment: ", comment.body)
-                return True
+            if re.search(trigger,comment.body,re.IGNORECASE):           
+                return flag + reply + " /n/n "
     return flag
 
 # Get the top 5 posts from the subreddit
@@ -80,16 +78,21 @@ for submission in subreddit.get_hot(limit=5):
     # get the comments
     comments = praw.helpers.flatten_tree(submission.comments)
     # flag variable to stay as true if the bot replies to any comments
-    flag_replied = False
+    reply_text = ""
     for comment in comments:
-        flag_replied = replyToComment(comment,["[[Demisexuality]]", "[[Demisexual]]"],"A demisexual is a person who may experience sexual attraction but only after forming a strong emotion connection with someone. [Learn More](https://www.reddit.com/r/demisexuality/comments/2osqfz/links_and_resources_masterpost/)",flag_replied)
-        flag_replied = replyToComment(comment,["[[Asexuality]]", "[[Asexual]]"],"An asexual is a person who does not experience sexual attraction. [Learn More](http://www.asexuality.org/home/?q=overview.html)",flag_replied)
-        flag_replied = replyToComment(comment,["[[Gray Asexuality]]", "[[Gray A]]", "[[Graysexual]]", "[[Grey Asexuality]]", "[[Grey A]]", "[[Greysexual]]"],"A grey asexual is a person at neither end of the spectrum on (a)sexual attraction. It can be used as an umbrella term for those who do not feel they fit as allosexual or asexual. [Learn More](http://www.asexuality.org/wiki/index.php?title=Gray-A_/_Grey-A)",flag_replied)
-        flag_replied = replyToComment(comment,["[[Autochorisexuality]]", "[[Autochoris]]", "[[Autochorisexual]]", "[[Autochorissexual]]", "[[Autochorissexuality]]"],"An autochorisexual person is in a subset of asexuality where there is a disconnect between oneself and a sexual target or object. For example a lack of desire to be a participant in sexual activies though still fantastising about sex. [Learn More](http://asexuals.wikia.com/wiki/Autochorissexual)",flag_replied)
-    # avoid replying to any more comments after this run
-    if flag_replied:
-        # Store the comment id into the list
-        replied_to.append(comment.id)
+        reply_text = add_reply_if_valid(comment,["[[Demisexuality]]", "[[Demisexual]]"],"A demisexual is a person who may experience sexual attraction but only after forming a strong emotion connection with someone. [Learn More](https://www.reddit.com/r/demisexuality/comments/2osqfz/links_and_resources_masterpost/)",reply_text)
+        reply_text = add_reply_if_valid(comment,["[[Asexuality]]", "[[Asexual]]"],"An asexual is a person who does not experience sexual attraction. [Learn More](http://www.asexuality.org/home/?q=overview.html)",reply_text)
+        reply_text = add_reply_if_valid(comment,["[[Gray Asexuality]]", "[[Gray A]]", "[[Graysexual]]", "[[Grey Asexuality]]", "[[Grey A]]", "[[Greysexual]]"],"A grey asexual is a person at neither end of the spectrum on (a)sexual attraction. It can be used as an umbrella term for those who do not feel they fit as allosexual or asexual. [Learn More](http://www.asexuality.org/wiki/index.php?title=Gray-A_/_Grey-A)",reply_text)
+        reply_text = add_reply_if_valid(comment,["[[Autochorisexuality]]", "[[Autochoris]]", "[[Autochorisexual]]", "[[Autochorissexual]]", "[[Autochorissexuality]]"],"An autochorisexual person is in a subset of asexuality where there is a disconnect between oneself and a sexual target or object. For example a lack of desire to be a participant in sexual activies though still fantastising about sex. [Learn More](http://asexuals.wikia.com/wiki/Autochorissexual)",reply_text)
+        # avoid replying to any more comments after this run
+        # if flag_replied is no longer empty
+        if not reply_text == "":
+            print ("Bot replying to comment: ", comment.body)
+            comment.reply(reply_text+END_OF_REPLY_MSG)
+            # Store the comment id into the list
+            replied_to.append(comment.id)
+            # limits on redidt api
+            break
 
 print("finished looking at comments")
 
