e87220a2b595dbfb9b3046e4472f795c8a159fb1
Skeletonxf <Skeletonxf@users.noreply.github.com>
2017-02-13 23:47:30 +0000

    Update script to new version of PRAW
    Need to re-add delayed post on rate limit exceeded once found in newer API

--- script.py
+++ script.py
@@ -40,11 +40,12 @@ if not os.path.isfile("config_bot.py"):
 
 print("loading definition bot")
 
-r = praw.Reddit(USER_AGENT)
+r = praw.Reddit(client_id=REDDIT_ID,
+                client_secret=REDDIT_SECRET,
+                refresh_token=REFRESH_TOKEN,
+                user_agent=USER_AGENT)
 
-r.set_oauth_app_info(REDDIT_ID,REDDIT_SECRET,REDDIT_URI)
-
-r.refresh_access_information(REFRESH_TOKEN)
+#r.refresh_access_information(REFRESH_TOKEN)
 
 print("logged in on OAuth")
 
@@ -62,71 +63,77 @@ else:
         # convert back to list
         replied_to = list(replied_to)
 
+print(replied_to)
+
 print("about to search for comments to reply to")
 
 def add_reply_if_valid(comment,triggers,reply,flag):
     "This checks for a comment in the comments to reply to and then replies avoiding quotes"
-    if comment.id not in replied_to:
-        for trigger in triggers:
-            # search using regex for the trigger in the comment body
-            for specific_trigger in re.finditer(trigger,comment.body,re.IGNORECASE):
-                # splice substring going from comment body start to the trigger text location
-                sub_comment = comment.body[:specific_trigger.start()]
-                last_line_break = -1
-                last_quote = -1
-                # loop through substring looking for occurances of newlines
-                for line_break in re.finditer("\n\n",sub_comment):
-                    last_line_break = line_break.start()
-                
-                # loop through substring looking for occurances of reddit quotes starting
-                for quote in re.finditer("> ",sub_comment):
-                    last_quote = quote.start()
-                
-                # if user's comment has no quotes in
-                if last_quote == -1:
+    for trigger in triggers:
+        # search using regex for the trigger in the comment body
+        for specific_trigger in re.finditer(trigger,comment.body,re.IGNORECASE):
+            # splice substring going from comment body start to the trigger text location
+            sub_comment = comment.body[:specific_trigger.start()]
+            last_line_break = -1
+            last_quote = -1
+            # loop through substring looking for occurances of newlines
+            for line_break in re.finditer("\n\n",sub_comment):
+                last_line_break = line_break.start()
+            
+            # loop through substring looking for occurances of reddit quotes starting
+            for quote in re.finditer("> ",sub_comment):
+                last_quote = quote.start()
+            
+            # if user's comment has no quotes in
+            if last_quote == -1:
+                return flag + reply + "\n\n"
+            else:
+                # if user's comment has quotes in but the quote ends before the trigger keyword
+                if not last_line_break == -1 and last_line_break > last_quote:
                     return flag + reply + "\n\n"
-                else:
-                    # if user's comment has quotes in but the quote ends before the trigger keyword
-                    if not last_line_break == -1 and last_line_break > last_quote:
-                        return flag + reply + "\n\n"
     return flag
 
 def post_definitions():
     "Gets the top 5 posts from the subreddit"
     for subreddit_name in SUBREDDITS:
-        subreddit = r.get_subreddit(subreddit_name)
-        for submission in subreddit.get_hot(limit=5):
+        subreddit = r.subreddit(subreddit_name)
+        for submission in subreddit.hot(limit=5):
             # get the comments
-            comments = praw.helpers.flatten_tree(submission.comments)
+            comments = submission.comments.list()
             # flag variable to stay as true if the bot replies to any comments
             reply_text = ""
             for comment in comments:
-                # avoid replying in more comments
-                if isinstance(comment, praw.objects.Comment):
-                    reply_text = add_reply_if_valid(comment,["\[\[Demisexuality\]\]", "\[\[Demisexual\]\]"],"A demisexual is a person who may experience sexual attraction but only after forming a strong emotion connection with someone. [Learn More](https://www.reddit.com/r/demisexuality/comments/2osqfz/links_and_resources_masterpost/)",reply_text)
-                    reply_text = add_reply_if_valid(comment,["\[\[Asexuality\]\]", "\[\[Asexual\]\]"],"An asexual is a person who does not experience sexual attraction. [Learn More](http://www.asexuality.org/home/?q=overview.html)",reply_text)
-                    reply_text = add_reply_if_valid(comment,["\[\[Gr[ae]y Asexuality\]\]", "\[\[Gr[ae]y A\]\]", "\[\[Gr[ae]ysexual\]\]"],"A grey asexual is a person at neither end of the spectrum on (a)sexual attraction. It can be used as an umbrella term for those who do not feel they fit as allosexual or asexual. [Learn More](http://www.asexuality.org/wiki/index.php?title=Gray-A_/_Grey-A)",reply_text)
-                    reply_text = add_reply_if_valid(comment,["\[\[Autochorisexuality\]\]", "\[\[Autochoris\]\]", "\[\[Autochorisexual\]\]", "\[\[Autochorissexual\]\]", "\[\[Autochorissexuality\]\]"],"An autochorisexual person is in a subset of asexuality where there is a disconnect between oneself and a sexual target or object. For example a lack of desire to be a participant in sexual activies though still fantastising about sex. [Learn More](http://asexuals.wikia.com/wiki/Autochorissexual)",reply_text)
-                    # avoid replying to any more comments after this run
-                    # if flag_replied is no longer empty
-                    if not reply_text == "":
-                        print ("Bot replying to comment: ", comment.id)
-                        # limits on reddit api
-                        try:
-                            comment.reply(reply_text+END_OF_REPLY_MSG)
-                            # Store the comment id into the list
-                            replied_to.append(comment.id)
-                        except praw.errors.RateLimitExceeded as e:
-                            print("Replying too soon!")
-                            print("Waiting " + str(e.sleep_time) + " seconds")
-                            time.sleep(e.sleep_time)
-                            comment.reply(reply_text+END_OF_REPLY_MSG)
-                            replied_to.append(comment.id)
-                        # at most reply once per run
-                        return
-
+                if comment.id not in replied_to:
+                    # avoid replying in more comments
+                    if isinstance(comment, praw.models.Comment):
+                        reply_text = add_reply_if_valid(comment,["\[\[Demisexuality\]\]", "\[\[Demisexual\]\]"],"A demisexual is a person who may experience sexual attraction but only after forming a strong emotion connection with someone. [Learn More](https://www.reddit.com/r/demisexuality/comments/2osqfz/links_and_resources_masterpost/)",reply_text)
+                        reply_text = add_reply_if_valid(comment,["\[\[Asexuality\]\]", "\[\[Asexual\]\]"],"An asexual is a person who does not experience sexual attraction. [Learn More](http://www.asexuality.org/home/?q=overview.html)",reply_text)
+                        reply_text = add_reply_if_valid(comment,["\[\[Gr[ae]y[ \-]Asexuality\]\]", "\[\[Gr[ae]y[ \-]A\]\]", "\[\[Gr[ae]ysexual\]\]"],"A grey asexual is a person at neither end of the spectrum on (a)sexual attraction. It can be used as an umbrella term for those who do not feel they fit as allosexual or asexual. [Learn More](http://www.asexuality.org/wiki/index.php?title=Gray-A_/_Grey-A)",reply_text)
+                        reply_text = add_reply_if_valid(comment,["\[\[Autochorisexuality\]\]", "\[\[Autochoris\]\]", "\[\[Autochorisexual\]\]", "\[\[Autochorissexual\]\]", "\[\[Autochorissexuality\]\]"],"An autochorisexual person is in a subset of asexuality where there is a disconnect between oneself and a sexual target or object. For example a lack of desire to be a participant in sexual activies though still fantastising about sex. [Learn More](http://asexuals.wikia.com/wiki/Autochorissexual)",reply_text)
+                        reply_text = add_reply_if_valid(comment,["\[\[Bisexuality\]\]", "\[\[Bisexual\]\]", "\[\[Bi\]\]"],"A bisexual person is a person who experiences sexual attraction to at least two genders. Some may be only attracted to men/women while others may consider themselves attracted to same sex and different sex - not excluding minority genders. [~~Learn More~~](https://www.reddit.com/r/SexualityDefBot/comments/510m0s/definitions/)",reply_text)
+                        reply_text = add_reply_if_valid(comment,["\[\[Pansexuality\]\]", "\[\[Pansexual\]\]", "\[\[Pan\]\]"],"A pansexual person is a person who experiences sexual attraction irrespective of gender / to all genders. Some may consider themseles to be gender blind as apposed to gender attracted or to be attracted to a person's personality rather than gender or appearence. [~~Learn More~~](https://www.reddit.com/r/SexualityDefBot/comments/510m0s/definitions/)",reply_text)
+                        # avoid replying to any more comments after this run
+                        # if flag_replied is no longer empty
+                        if not reply_text == "":
+                            print("Bot replying to comment: " + comment.id)
+                            # limits on reddit api
+                            try:
+                                comment.reply(reply_text+END_OF_REPLY_MSG)
+                                # Store the comment id into the list
+                                replied_to.append(comment.id)
+                            except praw.exceptions.APIException as e:
+                                print("Replying too soon or to an out of date comment!")
+                                # TODO get sleep time for rate limit again
+                                #print("Waiting " + str(e.sleep_time) + " seconds")
+                                #time.sleep(e.sleep_time)
+                                #comment.reply(reply_text+END_OF_REPLY_MSG)
+                                #replied_to.append(comment.id)
+                            # at most reply once per run
+                            return
+                else:
+                    print("Already replied to comment: " + comment.id)
 post_definitions()
-                        
+                 
 print("finished looking at comments")
 
 print("writing replies to file")
