8b9e039b26d71cf1fefbc95979d30c4184b00da2
Skeletonxf <skeletonxf@gmail.com>
2018-09-02 21:59:22 +0100

    Fix error in date format

--- weekly-topics.lua
+++ weekly-topics.lua
@@ -5,9 +5,9 @@ function topics.run()
   local topics = {
     ['August 26, 2018'] = "**The Weird Things Allosexuals Do**" .. newline ..
       "What things do allosexuals do that just confound you to no end?",
-    ['September 2, 2018'] = "**Labels before there were labels**" .. newline ..
+    ['September 02, 2018'] = "**Labels before there were labels**" .. newline ..
       "Before you discovered the term Demisexuality, did you go by a different label it descriptor? Did you try to fit into another label, or did you stand apart?",
-    ['September 9, 2018'] = "**Man's Best Friends?**" .. newline ..
+    ['September 09, 2018'] = "**Man's Best Friends?**" .. newline ..
       "How is your relationship with animals when compared to people, easier, harder, about the same? Are animals something you can bond with other people over?",
     ['September 16, 2018'] = "**Freaky Friday**" .. newline ..
       "If you could live on the other side for one day/night, as an Allosexual, would you? What would you do?",
