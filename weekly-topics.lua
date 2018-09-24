d822a711c15d2b1ecabf27468b9751a186db3e03
Skeletonxf <skeletonxf@gmail.com>
2018-09-24 21:00:04 +0100

    Add more weekly topics

--- weekly-topics.lua
+++ weekly-topics.lua
@@ -11,6 +11,15 @@ function topics.run()
       "How is your relationship with animals when compared to people, easier, harder, about the same? Are animals something you can bond with other people over?",
     ['September 16, 2018'] = "**Freaky Friday**" .. newline ..
       "If you could live on the other side for one day/night, as an Allosexual, would you? What would you do?",
+    ['September 23, 2018'] = "**Loud And Proud**" .. newline ..
+      "Do you tell people you're Demisexual, do people in general know what it means?",
+    ['September 30, 2018'] = "**Gossip Girl**" .. newline ..
+      "How do you feel when friends talk about their relationships and encounters? Do you join in, sit back and listen, or try to change the topic of conversation?",
+    ['October 07, 2018'] = "**Shades Of Gray**" .. newline ..
+      "What do you see as the difference between Gray-Asexual and Demisexual, being as they are so closely related?" .. newline ..
+      "http://wiki.asexuality.org/Gray-A/Grey-A",
+    ['October 14, 2018'] = "**A Demisexual Mascot**" .. newline ..
+      'Some in the Asexual community see Dragons as their "Mascot" of sorts. If Demisexuals had a "Mascot", what would it be?',
   }
   for week, topic in pairs(topics) do
     if containsIgnoreCase(title, week) then
